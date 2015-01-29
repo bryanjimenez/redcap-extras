4e1cce5061b3519f2a99570113c692ab5a28273f
Bryan <bryanjimenez6@gmail.com>
2015-01-29 15:12:46 -0500

    added error checking for missing parameters

--- hooks/resources/tooltip/tooltip.js
+++ hooks/resources/tooltip/tooltip.js
@@ -1,27 +1,32 @@
 function REDCap_Tooltip(fields) {
-	$.each(fields,function(key,data){
-		var tr = $('tr[sq_id='+key+']');
-		
-		// Replace term from note
-		var $note = $('div.note', tr);
-		$note.text($note.text().replace(data.term+"="+data.params, ''));
-		
-		var $element = $note.prev();
-		var $tooltip = $(document.createElement("a"));
-		$element.after($tooltip);
+  $.each(fields,function(key,data){
+    // skip if there is no tooltip message for the function
+    if(data.params){
+      var tr = $('tr[sq_id='+key+']');
 
-		$tooltip
-			.attr("title",data.params)
-			.text(" ★")
-			.css("cursor", "pointer")
-			.css('text-decoration', 'none');
-			
-		$tooltip.tooltip({ 
-							relative: true, 
-							effect: "fade", 
-							position: "center right", 
-							events: { def: "click,mouseout" }, 
-							tipClass: "tooltip"
-						});							
-	});
+      // Replace term from note
+      var $note = $('div.note', tr);
+      $note.text($note.text().replace(data.term+"="+data.params, ''));
+
+      var $element = $note.prev();
+      var $tooltip = $(document.createElement("a"));
+      $element.after($tooltip);
+
+      $tooltip
+        .attr("title",data.params)
+        .text(" ★")
+        .css("cursor", "pointer")
+        .css('text-decoration', 'none');
+
+      $tooltip.tooltip({
+        relative: true,
+        effect: "fade",
+        position: "center right",
+        events: { def: "click,mouseout" },
+        tipClass: "tooltip"
+      });
+    }else{
+      console.warn(key+" missing parameters in "+data.term);
+    }
+  });
 }
