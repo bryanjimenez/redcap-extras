61bcf192c716b80c94174777b039c32672bf7659
Bryan <bryanjimenez6@gmail.com>
2015-02-18 11:33:50 -0500

    tooltip cleanup spacing, remove duplicate line

--- hooks/resources/tooltip/tooltip.js
+++ hooks/resources/tooltip/tooltip.js
@@ -3,21 +3,20 @@ function REDCap_Tooltip(fields) {
     // skip if there is no tooltip message for the function
     if(data.params){
       var tr = $('tr[sq_id='+key+']');
-
+      
       // Replace term from note
       var $note = $('div.note', tr);
       $note.text($note.text().replace(data.term+"="+data.params, ''));
-
+      
       var $element = $note.prev();
       var $tooltip = $(document.createElement("a"));
       $element.after($tooltip);
-
+      
       $tooltip
         .attr("title",data.params)
         .text(" ★")
         .addClass("toolTipIcon");
-        .addClass("toolTipIcon");
-        
+      
       $tooltip.tooltip({
         relative: true,
         effect: "fade",
